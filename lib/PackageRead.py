e54a99741374e32166bd2a20dcd5011a269c400a
Skip Grube <sgrube@ciq.com>
2024-01-26 22:58:34 -0500

    Enhancements, including:
    - Fast module support from module_base
    - timestamp filter support
    - minor cleanup
    
    -Skip

--- lib/PackageRead.py
+++ lib/PackageRead.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 
 import dnf
+import dnf.module.module_base
+import hawkey
+from collections import OrderedDict
 
 import os,shutil,subprocess,sys
 
+
+
+def _profile_comparison_key(profile):
+    return profile.getName()
 
 # Configure DNF for a set of repositories, and load all package information into memory (returned as an object)
 # We'll compare these objects (multiple sets of repos) in another class to determine changes, CVEs fixed, changelogs, etc.
@@ -14,12 +21,17 @@ import os,shutil,subprocess,sys
 class PackageRead:
 
     pkg = []
+    dnfBase = set()
+    buildTime = 0
     
-    def __init__(self, repoList, latest):
+    def __init__(self, repoList, latest, bTime):
         if os.path.exists('/tmp/temp_dnf_cache'):
             shutil.rmtree('/tmp/temp_dnf_cache')
-        dnfBase = dnf.Base()
-        dnfConf = dnfBase.conf
+        
+        self.buildTime = bTime
+
+        self.dnfBase = dnf.Base()
+        dnfConf = self.dnfBase.conf
 
         dnfConf.gpgcheck=False
         dnfConf.reposdir="/tmp"
@@ -29,32 +41,35 @@ class PackageRead:
         # Add all the repos to our dnf configuration.  Each one is designated "repoId_#", with the passed in list of URLs
         repos=[]
         for r in range(0, len(repoList)):
-            repos.append(dnfBase.repos.add_new_repo("repoId_" + str(r), dnfConf, baseurl=[str(repoList[r])]))
+            repos.append(self.dnfBase.repos.add_new_repo("repoId_" + str(r), dnfConf, baseurl=[str(repoList[r])]))
             repos[r].load_metadata_other = True
             repos[r].module_hotfixes = True    
         
-        dnfBase.read_all_repos()
-        dnfBase.fill_sack(load_system_repo=False)
-
-        # Gather list of all packages in the repo to a temporary list:
-        tmpPkgList = dnfBase.sack.query().available().filter()
+        self.dnfBase.read_all_repos()
+        self.dnfBase.fill_sack(load_system_repo=False)
         
-        # Get a mapping of modular package -> module stream information for lookup
-        #moduleMap = 
+        # Gather list of all packages in the repo to a temporary list:
+        tmpPkgList = self.dnfBase.sack.query().available().filter()
+        
         
         # Main loop: Go through every package we collected from our repos, find the matching source package, and create the de-duplicated pkg[] list
         # (with the source name as canonical name, and data tagging the module label, if any)
         for i in tmpPkgList:
-            i.module_label=""
+            i.module_label="-"
           #i.cve_list=[]
-
+            
+            # Skip any package which falls outside our buildTime criteria:
+            if i.buildtime < self.buildTime:
+                continue
+                        
+            
             # If the binary package is a child package of source w/ same version, then we will use that srpm name instead:
             if str(i.version + "-" + i.release) in str(i.sourcerpm):
                 i.rName = i.source_name
             else:
                 i.rName = i.name
           
-            # If we've already done this source RPM, skip to the next.  We don't want duplicates:
+            # If we've already done this same version src rpm, skip to the next.  We don't want duplicates:
             pkgFound = False
             for p in self.pkg:
                 if i.rName == p.rName and i.version == p.version and i.release == p.release:
@@ -118,7 +133,42 @@ class PackageRead:
   #  print(str(j))
 
     # Given a set of repos and a package, find out which module this package belongs to:
+    # Logic for this is lifted from _what_provides() in DNF's own module_base.py
     def getModuleLabel(self, package, repos):     
+        modulePackages = self.dnfBase._moduleContainer.getModulePackages()
+        baseQuery = self.dnfBase.sack.query().filterm(empty=True).apply()
+        getBestInitQuery = self.dnfBase.sack.query(flags=hawkey.IGNORE_MODULAR_EXCLUDES)
+        
+
+        subj = dnf.subject.Subject(str(package.name + '-' + package.version + '-' + package.release))
+        baseQuery = baseQuery.union(subj.get_best_query(
+            self.dnfBase.sack, with_nevra=True, with_provides=False, with_filenames=False,
+            query=getBestInitQuery))
+
+        baseQuery.apply()
+
+        for modulePackage in modulePackages:
+            artifacts = modulePackage.getArtifacts()
+            if not artifacts:
+                continue
+            query = baseQuery.filter(nevra_strict=artifacts)
+            if query:
+                for pkg in query:
+                    string_output = ""
+                    profiles = []
+                    for profile in sorted(modulePackage.getProfiles(), key=_profile_comparison_key):
+                        if pkg.name in profile.getContent():
+                            profiles.append(profile.getName())
+                    lines = OrderedDict()
+                    print("DEBUG ::  package " +  package.name + '-' + package.version + '-' + package.release + "    found to be part of module  "  + str(modulePackage.getFullIdentifier().split(":")[0] + ":" + modulePackage.getFullIdentifier().split(":")[1]))
+                    #lines["Module"] = modulePackage.getFullIdentifier()
+                    #lines["Profiles"] = " ".join(sorted(profiles))
+                    #lines["Repo"] = modulePackage.getRepoID()
+                    #lines["Summary"] = modulePackage.getSummary()
+                    return str(modulePackage.getFullIdentifier().split(":")[0] + ":" + modulePackage.getFullIdentifier().split(":")[1])
+        
+        
+        
         reposFromPath = ""
         repoIds = ""
         for r in repos:
@@ -135,6 +185,7 @@ class PackageRead:
             
         # If we somehow didn't find a "Module   :" line in DNF output, return empty
         return ""
+
 
   #pp.pprint(i.changelogs)
 
--- pkgview.py
+++ pkgview.py
@@ -2,10 +2,31 @@
 
 
 from lib.PackageRead import *
+import time,datetime,sys
+
 
 # Read packages from these repos, only grab latest:
 repos = ["http://dl.rockylinux.org/pub/rocky/8/BaseOS/x86_64/os/","http://dl.rockylinux.org/pub/rocky/8/AppStream/x86_64/os/"]
-packageRepo1 = PackageRead(repos, True)
+
+daysAgo = 0
+buildDate = ""
+buildTime = 0
+
+buildDate = "2023-11-30"
+
+
+# Calculate the "origin point": any packages built before this time are not checked for changes
+if buildDate != "":
+    buildTime = int(datetime.datetime.strptime(buildDate, "%Y-%m-%d").timestamp())
+
+if int(daysAgo) > 0:
+    buildTime = int(time.time()) - int(daysAgo * 86400)
+
+if buildDate == "" and int(daysAgo) <= 0:
+    print("Error, *must* specify --days  or --date YYYY-MM-DD  on the command line!")
+    sys.exit(1)
+
+packageRepo1 = PackageRead(repos, True, buildTime)
 
 pkgGroup1 = packageRepo1.pkg
 
@@ -28,7 +49,7 @@ for p in packageRepo1.pkg:
     
     changes = "\"\"\"\n"
     for c in range(0, min(3, len(p.changelogs))):
-        changes += str(p.changelogs[c]["text"]) + "\n"
+        changes += str(p.changelogs[c]["text"]) + "  (DATE :: " + str(p.changelogs[c]["timestamp"]) + ")\n"
     changes += "\"\"\""
     
     print("N V R == " + p.rName + "-"  + p.version + "-" + p.release +  "\n\tRemote URL:" + str(p.remote_location())  + "\n\tmodule_label == " + p.module_label + "\n\tBuildTime == " + str(p.buildtime) +  "\n\tChangelog == \n" + str(changes) +  "\n\n")
